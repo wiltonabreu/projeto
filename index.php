68d58e203e5a47e64f6544343cb056910bd91f8d
wiltonabreu <wiltonabreuu@gmail.com>
2022-11-27 18:24:24 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Wap\Page;
+use \Wap\PageAdmin;
+use \Wap\Model\User;
 
 $app = new Slim();
 
@@ -16,6 +18,47 @@ $app->get('/', function() {
 	$page->setTpl("index");
 
 });
+
+//ROTA ADMIN
+$app->get('/admin/', function() {
+    User::verifyLogin();
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+//Rota para acessar a pagina de login
+$app->get('/admin/login', function() {
+    
+    $page = new PageAdmin([
+        "header"=>false,
+		"footer"=>false
+    ]);
+
+	$page->setTpl("login");
+
+});
+
+//Rota para receber dados da pagina de login
+$app->post('/admin/login', function() {
+
+    User::login($_POST["login"], $_POST["password"]);
+
+    header("Location: /admin");
+
+    exit;
+    
+});
+
+$app->get('/admin/logout', function() {
+    
+    User::logout();
+    header("Location: /admin");
+    exit;	
+
+});
+
 
 $app->run();
 
---
+++ vendor/wap/php-classes/src/PageAdmin.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Wap;
+
+use Rain\Tpl;
+
+class PageAdmin extends Page{
+
+	public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+	{
+
+		parent::__construct($opts, $tpl_dir);
+	}
+
+
+	
+
+}
+
+ ?>
